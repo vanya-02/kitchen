f675c91ecec4e0f8a78655c40db1259c5454a47d
Vanya191 <waffle4everyone>
2021-10-12 04:54:14 +0300

    refactor main

---
+++ src/main.py
@@ -0,0 +1,26 @@
+x = open('./data/cooks.json', 'r')
+
+import cook
+import json
+
+from flask import Flask, request
+
+kitchen = Flask(__name__)
+
+order_list = []
+
+
+# 1. Add new order to order list
+# 2. Send order to chefs/preparation
+# 3. Register receive time and prepared order time
+@kitchen.route('/order', methods=['POST'])
+def order():
+	# Parse and add incoming POST orders to order_list
+	data = request.json
+	order_list.append(data)
+
+	return json.dumps(order_list)
+
+
+if __name__ == '__main__':
+	kitchen.run()
