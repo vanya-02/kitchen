4a873931be244eb6d898932b8c00a53d2c2e56cf
Yunus0497 <Mohammed@fixnix.co>
2021-11-01 08:56:51 +0530

    Kitchen changes included

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ RUN pip install --no-cache-dir --upgrade pip && \
 ENV SRC_DIR /usr/bin/src/webapp/src
 COPY src/* ${SRC_DIR}/
 WORKDIR $SRC_DIR
+COPY src/data/ ./data
 ENV PYTHONUNBUFFERED=1
 EXPOSE 5000
 CMD ["python", "main.py"]
--- src/main.py
+++ src/main.py
@@ -13,17 +13,16 @@ order_list = []
 # 3. Register receive time and prepared order time
 @kitchen.route('/order', methods=['POST'])
 def order():
-	# Parse and add incoming POST orders to order_list
-	data = request.json
-	order_list.append(data)
-	print('Received order:', flush=False)
-	print(data, flush=False)
-	s = json.dumps(cook.prepare_oder(order_list.pop()))
-	requests.post('http://localhost:5050/distribution', json=s)
+    # Parse and add incoming POST orders to order_list
+    data = request.json
+    order_list.append(data)
+    print('Received order:', flush=False)
+    print(data, flush=False)
+    s = json.dumps(cook.prepare_oder(order_list.pop()))
+    requests.post('http://172.17.0.3:5050/distribution', json=s)
 
-
-	return s
+    return s
 
 
 if __name__ == '__main__':
-	kitchen.run(host='localhost', port=5000)
+    kitchen.run(host='0.0.0.0', port=5000)
